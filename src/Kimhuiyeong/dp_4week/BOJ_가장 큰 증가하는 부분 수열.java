036ec8336f4a682e7112d595ebbc29495b15fb05
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-06-30 16:51:45 +0900

    Update and rename 가장 큰 증가하는 부분 수열.java to BOJ_가장 큰 증가하는 부분 수열.java

--- src/Kimhuiyeong/dp_4week/BOJ_가장 큰 증가하는 부분 수열.java
+++ src/Kimhuiyeong/dp_4week/BOJ_가장 큰 증가하는 부분 수열.java
@@ -8,27 +8,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         // DP 배열 초기화
         int[] dp = new int[N];
         for (int i = 0; i < N; i++) {
-            dp[i] = A[i];
+            dp[i] = arr[i];
         }
 
         // DP 배열 계산
         for (int i = 1; i < N; i++) {
-            for (int j = 0; j < i; j++) {
-                if (A[j] < A[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + A[i]);
+            for (int j = 0; j < i; j++) { // i 이전 배열 탐색
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]); 
                 }
             }
         }
-        
+
         int maxSum = 0;
         for (int i = 0; i < N; i++) {
             maxSum = Math.max(maxSum, dp[i]);
