1f6973c677162fb19adb5adb7bd6bc65dd3800af
YunYeEun-124 <142516460+YunYeEun-124@users.noreply.github.com>
2024-08-11 13:56:17 +0900

    Create BOJ_별찍기.java

---
+++ src/YunYeEun/recursive_8week/BOJ_별찍기.java
@@ -0,0 +1,59 @@
+package etc;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+ 
+public class Main {
+	static char[][] matrix;
+	static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N = Integer.parseInt(br.readLine());
+ 
+		matrix = new char[N][N];
+        
+		star(0, 0, N, false);
+ 
+		//output
+		for (int i = 0; i < N; i++) {
+			bw.write(matrix[i]);
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+ 
+	static void star(int x, int y, int n, boolean blank) {
+ 
+		// 공백
+		if (blank) {
+			for (int i = x; i < x + n; i++) {
+				for (int j = y; j < y + n; j++) {
+					matrix[i][j] = ' ';
+				}
+			}
+			return;
+		}
+		//base case
+		if (n == 1) {
+			matrix[x][y] = '*';
+			return;
+		}
+		//recursive case
+		int nowsize = n/ 3;
+		int count = 0;
+		for (int i = x; i < x + n; i += nowsize) {
+			for (int j = y; j < y + n; j += nowsize) {
+				count++;
+				if (count == 5) { // 공백 칸일 경우
+					star(i, j, nowsize, true);
+				} else {
+					star(i, j, nowsize, false);
+				}
+			}
+		}
+	}
+}
