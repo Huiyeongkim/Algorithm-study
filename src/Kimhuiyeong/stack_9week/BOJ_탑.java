da980ef7c4b0f8d333d167013d4cd9261428148f
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-08-19 21:00:01 +0900

    Update BOJ_탑.java

--- src/Kimhuiyeong/stack_9week/BOJ_탑.java
+++ src/Kimhuiyeong/stack_9week/BOJ_탑.java
@@ -1,1 +1,30 @@
-f
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Stack<Integer[]> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            while (!stack.isEmpty()) {
+                if (stack.peek()[0]>a) {
+                    System.out.print(stack.peek()[1] + " ");
+                    break;
+                }
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                System.out.print(0+" ");
+            }
+            stack.push(new Integer[]{a, i + 1});
+        }
+    }
+}
